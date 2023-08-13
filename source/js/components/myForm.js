96ed0e5ca5b08b84448ddbe5aa40b60ff4385337
roman <romasaab@gmail.com>
2023-08-14 00:32:03 +0300

    add js functions

--- source/js/components/myForm.js
+++ source/js/components/myForm.js
@@ -1,24 +1,78 @@
 import vars from "../_vars";
+import { addCustomClass, removeCustomClass } from "../functions/customFunctions";
 const { form } = vars;
+import { modalClickHandler } from "./modals";
 
-form.addEventListener('submit', function(e){
+const btn = form.querySelector('.main-button');
+
+const itemLength = document.querySelectorAll('.form-list__item').length+1;
+const errorWrapeer = document.querySelector('.error');
+
+btn.addEventListener('click' , function(e){
     e.preventDefault();
-  const data = new FormData(form);
-  //const req = new XMLHttpRequest();
-  //req.open('POST', 'http://example.com');
-  //req.send(data);
 
-  console.log(getQueryString(data))
+    const data = {};
+    const errorArray = [];
+
+    for(let i=1; i < itemLength; i++){
+      const value = getCurrentValue(`ques${i}`);
+      // console.log(value);
+      if(value !== undefined){
+        data[`question_${i}`] = value;
+        // localStorage.setItem("testResault", JSON.stringify(data));
+        
+      } else {
+        errorArray.push(i);
+
+        // console.log(errorArray);
+      }
+    }
+
+    if(errorArray.length){
+      printError(errorArray);
+      return
+    }
+    modalClickHandler('entry', 'active');
+    console.log(data);
 })
 
-function getQueryString(formData){
-  let pairs = [];
+function printError(array){
+  // console.log(array);
+  errorWrapeer.querySelector('span').innerHTML = array;
+  addCustomClass(errorWrapeer, 'show')
 
-  for (let [key, value] of formData.entries()) {
-    pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
-  }
-  return pairs.join('&');
+  setTimeout(function(){
+    removeCustomClass(errorWrapeer, 'show')
+  }, 3000)
 }
+
+function getCurrentValue(name){
+  const inputs = document.getElementsByName(name);
+
+  for (const input of inputs) {
+    if(input.checked){
+
+      switch(input.value){
+        case '1':
+          return 'Так';
+          break;
+
+        case '2':
+          return 'Важко відповісти';
+          break;
+
+        case '3':
+          return 'Ні';
+          break;
+
+        default:
+          return null; 
+      }     
+    }
+  }
+}
+
+
 
 
 
--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -5,12 +5,14 @@ const { mainSlider } = vars;
 
 if(mainSlider){
   const mainContainer = document.querySelector('.form-slider__container');
-
+  const btns = document.querySelectorAll('.form-slider__btn');
   const mainSwiper = new Swiper(mainContainer, {
   slidesPerView: 1,
   spaceBetween: 40,
   observer: true,
   observeParents: true,
+  autoHeight: true,
+  allowTouchMove: false,
 
   navigation: {
     nextEl: '.form-slider__btn--next',
@@ -20,9 +22,27 @@ if(mainSlider){
   pagination: {
     el: '.form-slider__pagination',
     type: "bullets",
-    clickable: true,
   },
 
 });
+
+btns.forEach(function(btn){
+  btn.addEventListener('click', function(e){
+    e.preventDefault;
+  
+    window.scroll({
+      top: 0,
+      left: 0
+    });
+  })
+})
 }
+
+
+
+
+
+
+
+
 
